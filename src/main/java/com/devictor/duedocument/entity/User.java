175b3306a9de751d4dd81fdcf113cbcbacf0b8bd
Victor Henrique <developmentuser002@gmail.com>
2025-08-10 00:00:49 -0300

    refactor(entity): Add Getters and Setter Methods in User entity

--- src/main/java/com/devictor/duedocument/entity/User.java
+++ src/main/java/com/devictor/duedocument/entity/User.java
@@ -55,4 +55,52 @@ public class User {
     public void preUpdate() {
         this.updatedAt = OffsetDateTime.now();
     }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public OffsetDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(OffsetDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public OffsetDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(OffsetDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
