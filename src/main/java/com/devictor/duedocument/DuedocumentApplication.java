591938c1b9c02019731318fa1a7e8e58f43bac5b
Victor Henrique <developmentuser002@gmail.com>
2025-08-09 23:52:20 -0300

    initial config

---
+++ src/main/java/com/devictor/duedocument/DuedocumentApplication.java
@@ -0,0 +1,13 @@
+package com.devictor.duedocument;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuedocumentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuedocumentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devictor/duedocument/DuedocumentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devictor.duedocument;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuedocumentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
