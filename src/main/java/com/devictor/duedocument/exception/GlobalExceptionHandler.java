c2b39b88e5d58f149b67d849907538930b37546e
Victor Henrique <developmentuser002@gmail.com>
2025-08-11 11:09:11 -0300

    feat(exception): Add GlobalExceptionHandler with handling of the MethodArgumentNotValIdException

---
+++ src/main/java/com/devictor/duedocument/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.devictor.duedocument.exception;
+
+import com.devictor.duedocument.exception.dto.InvalidParamDto;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DueDocumentException.class)
+    public ProblemDetail handleDueDocumentException(DueDocumentException e) {
+        return e.toProblemDetail();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        var invalidParams = e.getFieldErrors()
+                .stream()
+                .map(fe -> new InvalidParamDto(fe.getField(), fe.getDefaultMessage()))
+                .toList();
+        var pd = ProblemDetail.forStatus(400);
+        pd.setTitle("Invalid request parameters");
+        pd.setDetail("There is invalid fields on the request");
+        pd.setProperty("invalid-params", invalidParams);
+        return pd;
+    }
+}
